2f085d96ef926518a90340ae84b46f20f76217fa
Sérgio Veríssimo <sergio_verissimo_123@hotmail.com>
2024-02-18 00:55:10 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import "./App.css";
 import NavBar from "./components/Navbar/Navbar";
 // import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 import Background from "./components/Background/Background";
 import HomePage from "./pages/HomePage/HomePage";
 import ContactPage from "./pages/ContactPage/ContactPage";
@@ -10,7 +11,6 @@ import AboutPage from "./pages/AboutPage/AboutPage";
 import ExperiencePage from "./pages/ExperiencePage/ExperiencePage";
 import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 const App = () => {
   return (
