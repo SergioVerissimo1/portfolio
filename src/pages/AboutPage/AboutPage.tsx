2cae5e614dec64b53c56b0807bcd8980be642480
Sérgio Veríssimo <sergio_verissimo_123@hotmail.com>
2024-02-18 02:04:04 +0000

    Update AboutPage.tsx

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -8,15 +8,30 @@ const AboutPage = () => {
       id="about"
       content={
         <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            justifyContent: "space-between",
-          }}
+          style={
+            {
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }
+          }
         >
           <div>
             <PageTitle title="About Me" />
+          </div>
+          <div
+            style={{
+              textAlign: "left",
+              width: "60%",
+              color: "white",
+            }}
+          >
+            I'm Sérgio Veríssimo, a full-stack developer based in Lisbon,
+            Portugal. I'm passionate about creating beautiful, responsive and
+            user-friendly web applications. I'm always looking for new
+            challenges and opportunities to learn and grow. Feel free to explore
+            my work and get in touch with me!
           </div>
         </div>
       }
