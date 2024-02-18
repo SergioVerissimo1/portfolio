36fda73388804626b79f11f065d4942bfe88e80c
Sérgio Veríssimo <sergio_verissimo_123@hotmail.com>
2024-02-18 00:55:05 +0000

    Update Background.tsx

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 import { ParticlesConfig } from "./ParticlesConfig";
