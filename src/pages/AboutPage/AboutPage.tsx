785e3d952e78c304f3008a807b07118df1718f90
Sérgio Veríssimo <sergio_verissimo_123@hotmail.com>
2024-03-18 14:09:54 +0000

    WIP

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -19,16 +19,9 @@ const AboutPage = () => {
           <div>
             <PageTitle title="About Me" />
           </div>
-          <div
-            style={{ display: "flex", flexDirection: "row", width: "70rem" }}
-          >
-            <div
-              style={{
-                textAlign: "left",
-                width: "30rem",
-              }}
-            >
-              <p className={styles.paragraph} style={{marginTop: '1rem'}}>
+          <div className={styles.aboutContainer}>
+            <div className={styles.textContainer}>
+              <p className={styles.paragraph}>
                 I'm Sérgio Veríssimo, a full-stack Web Developer based in
                 Sesimbra, Portugal. I'm passionate about creating beautiful,
                 responsive and user-friendly web applications.
@@ -38,80 +31,59 @@ const AboutPage = () => {
                 and grow. Feel free to explore my work and get in touch with me!
               </p>
             </div>
-            <div
-              style={{
-                width: "36rem",
-                justifyContent: "end",
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <div className={styles.skillsRow}>
-                <Skill imageSrc="../../../logos-about/react.svg" name="React" />
-                <Skill
-                  imageSrc="../../../logos-about/nextjs.svg"
-                  name="Next.js"
-                />
-                <Skill
-                  imageSrc="../../../logos-about/javascript.svg"
-                  name="Javascript"
-                />
-                <Skill imageSrc="../../../logos-about/html5.svg" name="HTML5" />
-                <Skill imageSrc="../../../logos-about/css3.svg" name="CSS3" />
-              </div>
-              <div className={styles.skillsRow}>
-                {/* <Skill imageSrc="../../../logos-about/bootstrap.svg" name="Bootstrap" />
-              <Skill imageSrc="../../../logos-about/nodejs.svg" name="Node.js" />
-              <Skill imageSrc="../../../logos-about/express.svg" name="Express" /> */}
-                <Skill imageSrc="../../../logos-about/mysql.svg" name="MySQL" />
-                <Skill
-                  imageSrc="../../../logos-about/postgresql.svg"
-                  name="PostgreSQL"
-                />
-                <Skill imageSrc="../../../logos-about/git.svg" name="Git" />
-                <Skill
-                  imageSrc="../../../logos-about/aspnet.svg"
-                  name="ASP.net"
-                />
-                <Skill
-                  imageSrc="../../../logos-about/nodejs.svg"
-                  name="Node.js"
-                />
-              </div>
-              <div className={styles.skillsRow}>
-                <Skill
-                  imageSrc="../../../logos-about/adobexd.svg"
-                  name="Adobe XD"
-                />
-                <Skill
-                  imageSrc="../../../logos-about/angular.svg"
-                  name="Angular"
-                />
-                <Skill imageSrc="../../../logos-about/agile.svg" name="Agile" />
-                <Skill imageSrc="../../../logos-about/php.svg" name="PHP" />
-                <Skill
-                  imageSrc="../../../logos-about/flutter.svg"
-                  name="Flutter"
-                />
-              </div>
-              {/* <div className={styles.skillsRow}>
-                <Skill
-                  imageSrc="../../../logos-about/postman.svg"
-                  name="Postman"
-                />
-                <Skill
-                  imageSrc="../../../logos-about/angular.svg"
-                  name="Angular"
-                />
-                <Skill
-                  imageSrc="../../../logos-about/nodejs.svg"
-                  name="Node.js"
-                />
-                <Skill
-                  imageSrc="../../../logos-about/aspnet.svg"
-                  name="ASP.net"
-                />
-              </div> */}
+            <div className={styles.skillsContainer}>
+              <Skill imageSrc="../../../logos-about/react.svg" name="React" />
+              <Skill
+                imageSrc="../../../logos-about/nextjs.svg"
+                name="Next.js"
+              />
+              <Skill
+                imageSrc="../../../logos-about/javascript.svg"
+                name="Javascript"
+              />
+              <Skill
+                imageSrc="../../../logos-about/typescript.svg"
+                name="Typecript"
+              />
+              <Skill imageSrc="../../../logos-about/html5.svg" name="HTML5" />
+              <Skill imageSrc="../../../logos-about/css3.svg" name="CSS3" />
+              {/* <Skill imageSrc="../../../logos-about/bootstrap.svg" name="Bootstrap" /> */}
+              <Skill
+                imageSrc="../../../logos-about/express.svg"
+                name="Express"
+              />
+              <Skill imageSrc="../../../logos-about/mysql.svg" name="MySQL" />
+              <Skill
+                imageSrc="../../../logos-about/postgresql.svg"
+                name="PostgreSQL"
+              />
+              <Skill imageSrc="../../../logos-about/git.svg" name="Git" />
+              <Skill
+                imageSrc="../../../logos-about/aspnet.svg"
+                name="ASP.net"
+              />
+              <Skill
+                imageSrc="../../../logos-about/nodejs.svg"
+                name="Node.js"
+              />
+              <Skill
+                imageSrc="../../../logos-about/fastify.svg"
+                name="Fastify"
+              />
+              <Skill
+                imageSrc="../../../logos-about/adobexd.svg"
+                name="Adobe XD"
+              />
+              <Skill
+                imageSrc="../../../logos-about/angular.svg"
+                name="Angular"
+              />
+              <Skill imageSrc="../../../logos-about/agile.svg" name="Agile" />
+              <Skill imageSrc="../../../logos-about/php.svg" name="PHP" />
+              <Skill
+                imageSrc="../../../logos-about/flutter.svg"
+                name="Flutter"
+              />
             </div>
           </div>
         </div>
