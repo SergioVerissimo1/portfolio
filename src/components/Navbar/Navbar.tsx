ac371ad888f4d1af54d64bedd56a52350c4d2ee3
Sérgio Veríssimo <sergio_verissimo_123@hotmail.com>
2024-05-20 02:44:44 +0100

    Design fixes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,23 @@
-import styles from "./Navbar.module.css";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { useState } from "react";
-import NavBarLink from "../NavbarLink/NavbarLink";
+import styles from './Navbar.module.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { useState } from 'react';
+import NavBarLink from '../NavbarLink/NavbarLink';
 
 export default function NavBar() {
   return (
-    <Navbar expand="lg" /*fixed="top"*/ className={styles.navbar}>
-      <Container>
+    <Navbar /*expand="lg"*/ /*fixed="top"*/ className={styles.navbar}>
+      <div className={styles.navbarRow}>
         {/* <Navbar.Toggle aria-controls="basic-navbar-nav" className={styles.navbarToggle}/> */}
         {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-          {/* <Nav className="m-auto"> */}
-            <NavBarLink name="Home" id="#home" />
-            <NavBarLink name="About" id="#about" />
-            <NavBarLink name="Experience" id="#experience" />
-            <NavBarLink name="Projects" id="#projects" />
-            <NavBarLink name="Contact" id="#contact" />
-          {/* </Nav> */}
+        {/* <Nav className="m-auto"> */}
+        <NavBarLink name="Home" id="#home" />
+        <NavBarLink name="About" id="#about" />
+        <NavBarLink name="Experience" id="#experience" />
+        <NavBarLink name="Projects" id="#projects" />
+        <NavBarLink name="Contact" id="#contact" />
+        {/* </Nav> */}
         {/* </Navbar.Collapse> */}
-      </Container>
+      </div>
     </Navbar>
   );
 }
